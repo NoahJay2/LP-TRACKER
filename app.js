1ca5f1bf761da9309c9dcf884b90da0e504936fe
Noah Jay <Hinojosa213@gmail.com>
2026-06-17 22:20:18 -0500

    Updated mobile view discount form

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 // Bump on each deploy. Shown in the sidebar footer so you can confirm at a
 // glance which build is actually live (handy when cache / deploy is in doubt).
-const BUILD_VERSION = '2026-06-17.42';
+const BUILD_VERSION = '2026-06-17.43';
 
 const STORAGE_KEY = 'lumen-tracker-v1';
 const $ = (s, ctx = document) => ctx.querySelector(s);
